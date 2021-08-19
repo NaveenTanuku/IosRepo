e912b98601aef3b66c5484984f079c75e0d9806f
Naveen Tanuku <ntanuku96@gmail.com>
2021-08-19 09:22:18 -0500

    Initial commit

---
+++ FirstFile.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "Hello, playground"
+var a=10;
+var b=20;
+var sum=0;
+
+sum = a+b;
+print(sum);
+
+var st = "Raghavendra -- sanjay system is not working"
+print(str+"\n"+st)
